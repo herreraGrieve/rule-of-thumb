74cf9565346ca4e8852cf1231903a28059ba4b8f
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-18 22:37:18 -0500

    add dynamic header component

---
+++ vue/src/components/Heading/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'Heading',
+    render(createElement) {
+        return createElement('h' + this.level, this.$slots.default);
+    },
+    props: {
+        level: {
+            type: Number,
+            default: 6,
+            required: true,
+        },
+    },
+};
+</script>
