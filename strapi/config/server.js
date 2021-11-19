a0b4d6f08572edbe1251811025d9f06916d33769
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-18 20:39:25 -0500

    add strapi project

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'edc6925d8387e8bb3955bf6037526710'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e9049820-3bf3-47da-9cc5-c87e9e34e96f'
+};
