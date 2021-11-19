29f8a165942378eae208b9cf9c56cc9608c26315
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-18 21:04:58 -0500

    add global css

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
+    <BaseTemplate/>
   </div>
 </template>
 
 <script>
+import BaseTemplate from "./components/BaseTemplate";
 
 export default {
   name: "App",
+  components: {
+    BaseTemplate,
+  },
 };
 </script>
