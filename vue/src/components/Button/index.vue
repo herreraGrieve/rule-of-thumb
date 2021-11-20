ea417dcb89ebbe09d1aab5e642b96144b6d45b98
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-19 23:30:28 -0500

    add button component

---
+++ vue/src/components/Button/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <button type="button" @click="click" class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: 'Button',
+    props: {
+        click: {
+            type: Function,
+        }
+  }
+};
+</script>
+
+<style scoped>
+    .button{
+        background: var(--color-darker-background);
+        border: 1px solid white;
+        color: white;
+        padding: 1rem 2rem;
+        font-size: var(--text-2);
+        cursor: pointer;
+    }
+    .button:disabled{
+        background: var(--color-darker-gray-background);
+        cursor: auto;
+    }
+</style>
