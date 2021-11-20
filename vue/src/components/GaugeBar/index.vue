b6d3771c0c7d82c11d73765a0294a45f7dee5c3f
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-19 22:39:08 -0500

    add gauge bar component markup

---
+++ vue/src/components/GaugeBar/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container" :style="barSizes">
+        <div class="upBar">
+            <span aria-hidden="true"> {{ upvotes }} </span>
+            <p class="sr-only"> {{ upvoteLabel }} </p>
+        </div>
+        <div class="downBar">
+            <span aria-hidden="true"> {{ downvotes }} </span>
+            <p class="sr-only"> {{ downvoteLabel }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'GaugeBar',
+    props: {
+        upvoteLabel: {
+            type: String,
+            default: '',
+        },
+        downvoteLabel: {
+            type: String,
+            default: '',
+        },
+        upvotes: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+        downvotes: {
+            type: Number,
+            default: 0,
+            required: true,
+        },
+    },
+    computed:{
+        barSizes(){
+            return `grid-template-columns: ${this.upvotes}fr ${this.downvotes}fr`
+        }
+    }
+};
+</script>
+
+<style scoped>
+    .container{
+        display: grid;
+    }
+    .upBar{
+        background-color: green;
+    }
+    .downBar{
+        background-color: blue;
+    }
+    .sr-only {
+        /* hides content for screens but not for screen-readers */
+        position:absolute;
+        left:-10000px;
+        top:auto;
+        width:1px;
+        height:1px;
+        overflow:hidden;
+    }
+</style>
