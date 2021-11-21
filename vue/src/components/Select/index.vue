b4b67ee6c42e8de0cf0e60516645e9b8f2ba6583
herreraGrieve <pedro.h.grieve@gmail.com>
2021-11-21 15:28:13 -0500

    add custom select component

---
+++ vue/src/components/Select/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <select  v-model="input" @change="onChange" class="select">
+        <slot></slot>
+    </select>
+</template>
+
+<script>
+
+export default {
+    name: 'Select',
+    props: ['value'],
+    methods: {
+        onChange(e){
+            this.$emit("change",e.target.value);
+        }
+    },
+    computed: {
+        input: {
+            get() {
+                return this.value;
+            },
+            set(val) {
+                this.$emit('input', val);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+    .select{
+        background: white;
+        padding: 0.4rem 3rem;
+        border: 2px solid var(--color-darker-gray);
+    }
+    @media screen and (min-width:1010px) {
+        .select{
+            padding: 0.6rem 4rem;
+        }
+    }
+</style>
